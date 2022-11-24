537a3707ec10c084a35d5e5ff275622343c082f9
debylu <112035765+debylu@users.noreply.github.com>
2022-11-23 21:40:53 -0300

    Add files via upload

---
+++ aula noite/quemsomos.php
@@ -0,0 +1,30 @@
+<?php
+    require_once 'head.php';
+    require_once 'menu.php';
+?>     
+ <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8">
+              <h3>Conserta Tudo</h3>
+              
+                <div class="col-md-4">
+              <h3>A Oficina Conserta Tudo foi fundada em 1996. Há mais de 25 anos estabelecidos no mercado de oficinas automotivas. Acreditamos que a experiência, compromissos com a qualidade e respeito às relações contribuíram para firmar nossa permanência no mercado com a inauguração das novas unidades Freguesia e Tijuca.
+Toda essa conquista nos traz o orgulho e determinação em seguir com a proposta de sermos reconhecidos, por nossos clientes e parceiros</h3>
+              <p>Nossa oficina esta há mais de 30 anos no mercado trabalhando 
+                sério por você e sua segurança.</p>
+            </div>
+
+            <div class="col-md-3">
+                <h3>Contatos</h3>
+                <p>(21)3334-7458</p>
+                <p>(21)98776-3435</p>
+                <p>(21)99775-1255</p>
+            </div>
+
+            <div class="col-md-3">
+                <h3>Horário de Atendimento</h3>
+                <ul>
+                  <li>Segunda a Sexta - 08:00 às 17:00</li>
+                  <li>Sábado - 08:00 às 13:00</li>
+                </ul>
+            </div>
